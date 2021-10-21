40fd614641e8a1f9d28e64b8322870b5cd8c5169
Isaac-AAK <67994259+Isaac-AAK@users.noreply.github.com>
2021-10-20 21:25:02 -0400

    v1

---
+++ src/test/java/com/iaktech/gmailSender/GmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iaktech.gmailSender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
